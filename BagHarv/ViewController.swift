9c67e2b185653e64ee7ef4aa4899a09992d89d49
Grandon Lin <glin@grandons-macbook-pro.local>
2018-01-17 21:56:52 -0500

    Initial Commit

---
+++ BagHarv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BagHarv
+//
+//  Created by Grandon Lin on 2018-01-17.
+//  Copyright © 2018 Grandon Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
